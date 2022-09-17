cf15957777928cc1564f26df0b8af1d4743f5248
Pham Binh <binhpd.55cb@gmail.com>
2022-09-17 10:38:46 +0700

    first commit

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,57 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hello_project/screens/home_screen.dart';
+
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomeScreen(),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My tickets"),
+      ),
+      body: Center(child: _widgetOptions[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(FluentSystemIcons.ic_fluent_home_regular), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Tab3"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Tab3")
+    ],
+    ),
+    );
+  }
+
+  void _onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    print('${_selectedIndex}');
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,83 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hello_project/screens/ticket_view.dart';
+import '../utils/app_style.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Gap(40),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good morning", style: Styles.headLineStyle1),
+                        const Gap(5),
+                        Text("Book tickets", style: Styles.headLineStyle3),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/ic_circle.png"))),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Colors.blue),
+                      Text("Search", style: Styles.headLineStyle4,)
+
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Upcomming Flights", style: Styles.headLineStyle2),
+                    InkWell(
+                      onTap: () {
+                        print("Tapppp");
+                      },
+                        child: Text("View All", style: Styles.headLineStyle4))
+                  ],
+                ),
+
+              ],
+            ),
+          ),
+          const Gap(25),
+          TicketView()
+        ],
+      ),
+    );
+  }
+}
